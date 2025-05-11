50b2c4535d757a7fba97501700aaab264e7f67d0
Mojtaba Karimi <moji.k1381@gmail.com>
2025-05-11 17:22:12 +0330

    add grammar_check func and secre_keys file

--- prediText/Flask/main.py
+++ prediText/Flask/main.py
@@ -5,6 +5,7 @@ from .db import get_db
 import difflib
 import torch
 from sapling import SaplingClient
+from .secure_keys import sap_api_key
 
 bp = Blueprint('main', __name__)
 
@@ -18,3 +19,16 @@ bp = Blueprint('main', __name__)
 def index():
     result = None
     return render_template('main/index.html', result=result)
+
+
+def grammar_check(text, lang):
+    client = SaplingClient(api_key=sap_api_key)
+    edits = client.edits(f'{text}', lang=lang, session_id="test_session")
+    gt = copy(text)
+    if not edits['edits']:
+        return text + 'ok shod'
+    for i in edits["edits"]:
+        gt = gt.replace(gt[int(i['start']):int(i['end'])], i['replacement'])
+    grammar_text = gt
+    return grammar_text
+
