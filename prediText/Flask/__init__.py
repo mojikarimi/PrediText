8f234a08de535e9509cc6f819c0dcb5b068db8ab
Mojtaba Karimi <moji.k1381@gmail.com>
2025-05-11 19:25:04 +0330

    add translate func in main.py

--- prediText/Flask/__init__.py
+++ prediText/Flask/__init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask
-
+import pickle
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SECRET_KEY='dev',
@@ -18,3 +18,30 @@ def create_app(test_config=None):
     # db
     db.init_app(app)
     # main
+    app.register_blueprint(main.bp)
+    app.add_url_rule('/', endpoint='index')
+    # auto complete word
+    import pickle
+    main.swedish_words = pickle.load(open('datasets/auto_word_complete/swedish_words.sug', 'rb'))
+    main.english_words = pickle.load(open('datasets/auto_word_complete/english_words.sug', 'rb'))
+    main.russian_words = pickle.load(open('datasets/auto_word_complete/russian_words.sug', 'rb'))
+    main.french_words = pickle.load(open('datasets/auto_word_complete/french_words.sug', 'rb'))
+    main.spanish_words = pickle.load(open('datasets/auto_word_complete/spanish_words.sug', 'rb'))
+    main.german_words = pickle.load(open('datasets/auto_word_complete/german_words.sug', 'rb'))
+    # Predictive word
+    main.sv_model = pickle.load(open('models/swedish_model.pkl', 'rb'))
+    main.sv_token = pickle.load(open('models/swedish_model.pkl', 'rb'))
+    main.en_model = pickle.load(open('models/english_model.pkl', 'rb'))
+    main.en_token = pickle.load(open('models/english_tokenizer.pkl', 'rb'))
+    main.ru_model = pickle.load(open('models/russian_model.pkl', 'rb'))
+    main.ru_token = pickle.load(open('models/russian_tokenizer.pkl', 'rb'))
+    main.fr_model = pickle.load(open('models/french_model.pkl', 'rb'))
+    main.fr_token = pickle.load(open('models/french_tokenizer.pkl', 'rb'))
+    main.es_model = pickle.load(open('models/spanish_model.pkl', 'rb'))
+    main.es_token = pickle.load(open('models/spanish_tokenizer.pkl', 'rb'))
+    main.de_model = pickle.load(open('models/german_model.pkl', 'rb'))
+    main.de_token = pickle.load(open('models/german_tokenizer.pkl', 'rb'))
+    # grammar checker
+    # from happytransformer import TTSettings
+    # main.happy_tt = pickle.load(open('models/en_grammar_correction', 'rb'))
+    # main.args = TTSettings(num_beams=5, min_length=1)
--- prediText/Flask/main.py
+++ prediText/Flask/main.py
@@ -32,3 +32,35 @@ def grammar_check(text, lang):
     grammar_text = gt
     return grammar_text
 
+
+@bp.route('/translate', methods=('POST', 'GET'))
+def translate():
+    if request.method == 'POST':
+        text = request.form['text']
+        translate_language = request.form['translate_language']
+        orig_language = request.form['orig_language']
+        if orig_language in ['de', 'es', 'fr', 'sv','en']:
+            c = [text]
+            while True:
+                grammar_text = grammar_check(c[-1], orig_language)
+                if grammar_text.endswith('ok shod'):
+                    break
+                c.append(grammar_text)
+            grammar_text = c[-1]
+        # if orig_language == 'en':
+        #     grammar_text = happy_tt.generate_text(f"grammar: {text}", args=args).text
+        if orig_language == 'ru':
+            translate_text = ts.translate_text(text, from_language=orig_language, to_language=translate_language)
+            grammar_text = ts.translate_text(translate_text, from_language=translate_language,
+                                             to_language=orig_language)
+        else:
+            translate_text = ts.translate_text(grammar_text, from_language=orig_language,
+                                               to_language=translate_language)
+
+        result = {'text': text,
+                  'translate_language': translate_language,
+                  'translate_text': translate_text,
+                  'orig_language': orig_language,
+                  'grammar_text': grammar_text}
+        return render_template('main/index.html', result=result)
+    return redirect(url_for('index'))
